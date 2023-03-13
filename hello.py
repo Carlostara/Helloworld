a9d3a82866c24270cf2e72010d70ae57963181db
Catarazona <bafford14@gmail.com>
2023-03-12 19:18:50 -0500

    Probando commit en vsc

--- hello.py
+++ hello.py
@@ -8,11 +8,15 @@ b = 2
 
 print(b+a)
 
-a = 93
-b = 27
+a = 16
+b = 25
+c = 27
 if a > b:
-    print("a is greater than b")
-elif a < b:
-    print("a is less than b")
-else: 
-    print ("a is equal to b")    
+    if b > c:
+        print ("a is greater than b and b is greater than c")
+    else: 
+        print ("a is greater than b and less than c")
+elif a == b:
+    print ("a is equal to b")
+else:
+    print ("a is less than b")
