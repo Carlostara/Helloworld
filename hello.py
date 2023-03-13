5d8b2d47685008a770daa9889842de1086e8491c
Catarazona <bafford14@gmail.com>
2023-03-12 19:16:12 -0500

    primer commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from datetime import date
+print("la fecha de hoy es:" + str (date.today()))
+
+print("Hola mundo")
+
+a = 1
+b = 2
+
+print(b+a)
+
+a = 93
+b = 27
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else: 
+    print ("a is equal to b")    
